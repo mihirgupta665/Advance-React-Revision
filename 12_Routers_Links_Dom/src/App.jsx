2dfb70d9fb33abf6ff090b8dfe66b63a491d80aa
Mihir Gupta <mihirgupta665@gmail.com>
2026-03-16 20:57:01 +0530

    creating outlet router provier link and router object

--- 12_Routers_Links_Dom/src/App.jsx
+++ 12_Routers_Links_Dom/src/App.jsx
@@ -1,16 +1,16 @@
 // padding goes till 12 and margin goes till 20
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './components/Header/header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <h1 className="text-4xl text-center bg-green-500 p-12 m-20">Learning Routers</h1>
+        
         </>
     )
 }
--- 12_Routers_Links_Dom/src/main.jsx
+++ 12_Routers_Links_Dom/src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+
+// RouterProvider : is a functional component which provides routing and takes 1 prop i.e. router
+// createBrowserRouter : array of router object
+// router object : path, element(to render), children(nested routing)
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout/>,
+        children: [
+            {
+                path: "",
+                element: <Home/>
+            },
+            {
+                path: "about",
+                element: <About/>
+            }
+        ]
+    }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
+
