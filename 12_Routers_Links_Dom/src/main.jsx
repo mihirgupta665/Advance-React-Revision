e59f9a7d66ef100f7a4df00b7e8a39e8fbb2bc39
Mihir Gupta <mihirgupta665@gmail.com>
2026-03-17 03:49:50 +0530

    Create Browser Router using createRoutesFromElements

--- 12_Routers_Links_Dom/src/main.jsx
+++ 12_Routers_Links_Dom/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 // RouterProvider : is a functional component which provides routing and takes 1 prop i.e. router
 // createBrowserRouter : array of router object
@@ -26,11 +27,21 @@ const router = createBrowserRouter([
             },
             {
                 path: "contact",
-                element:
+                element: <Contact/>
             }
         ]
     }
 ])
+
+const router2 = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout/>} >
+            <Route path="" element={<Home/>}/>
+            <Route path="about" element={<About/>} />
+            <Route path="contact" element={<Contact />} />
+        </Route>
+    )
+)
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
