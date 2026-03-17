a2dc4ad06e60bbe765d47878dd3bdd311709052c
Mihir Gupta <mihirgupta665@gmail.com>
2026-03-17 12:53:32 +0530

    Github followers and profive avatar_url extraction

--- 12_Routers_Links_Dom/src/components/Github/Github.jsx
+++ 12_Routers_Links_Dom/src/components/Github/Github.jsx
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from "react"
 
 export default function Github(){
 
-    [data, setData] = useState([])
+    let [data, setData] = useState(null)
 
     useEffect(() => {
-        fetch("https://api.github.com/users/mihirgupta665")
+        fetch("https://api.github.com/users/mihirgupta665", {
+            headers: {
+                Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`
+            }
+        })
         .then((res) => res.json())
         .then((data) => {
-            setData(data)
             console.log(data)
+            setData(data)
         })
     }, [])
 
     return(
         <div className="m-4 p-4 text-center text-3xl text-white bg-gray-500">
-            Github Followers : {data.followers}
+            Github Followers : {data?.followers}
+            <img src={data?.avatar_url} alt="Git Picture" width={300} 
+            />
         </div>
     )
 }
