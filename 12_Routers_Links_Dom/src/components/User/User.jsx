7d6227d951106f050c64c7da1a9c71b17b879198
Mihir Gupta <mihirgupta665@gmail.com>
2026-03-17 11:04:11 +0530

    Parmeter extraction from user component

---
+++ 12_Routers_Links_Dom/src/components/User/User.jsx
@@ -0,0 +1,20 @@
+// main.jsx define the path along with parameter name and render the element in which parameter is needed
+// <Route path="user/:userid" element={<User />} />
+// import call and use useParam to extract the parameter in the corresponding Element
+// const userid = useParams()
+// use the extracted parameter with the same name anywhere in your jsx file
+// <div> User: {userid} <div/>
+
+
+import {useParams} from "react-router-dom"
+
+export default function User(){
+
+    const userid = useParams()
+
+    return(
+        <div>
+            User: {userid}
+        </div>
+    )
+}
--- 12_Routers_Links_Dom/src/main.jsx
+++ 12_Routers_Links_Dom/src/main.jsx
@@ -7,6 +7,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/user.jsx'
 
 // RouterProvider : is a functional component which provides routing and takes 1 prop i.e. router
 // createBrowserRouter : array of router object
@@ -39,6 +40,7 @@ const router2 = createBrowserRouter(
             <Route path="" element={<Home/>}/>
             <Route path="about" element={<About/>} />
             <Route path="contact" element={<Contact />} />
+            <Route path="user/:userid" element={<User />} />
         </Route>
     )
 )
