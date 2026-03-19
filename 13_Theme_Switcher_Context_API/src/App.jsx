b15cb2b2228d4f47ebf5db2379ab876efe66f68d
Mihir Gupta <mihirgupta665@gmail.com>
2026-03-19 15:00:52 +0530

    Theme Switcher Project initialization

---
+++ 13_Theme_Switcher_Context_API/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <h1 className='bg-pink-500 p-4'>Welecome to Learn Theme Switcher</h1>
+        </>
+    )
+}
+
+export default App
