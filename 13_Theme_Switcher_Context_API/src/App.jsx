2735175233bb30d85580f28cf2ab7654aa7d7c21
Mihir Gupta <mihirgupta665@gmail.com>
2026-03-19 15:29:01 +0530

    APP UI created

--- 13_Theme_Switcher_Context_API/src/App.jsx
+++ 13_Theme_Switcher_Context_API/src/App.jsx
@@ -5,9 +5,19 @@ import './App.css'
 function App() {
 
     return (
-        <>
-            <h1 className='bg-pink-500 p-4'>Welecome to Learn Theme Switcher</h1>
-        </>
+
+        <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+
+                </div>
+
+                <div className="w-full max-w-sm mx-auto">
+
+                </div>
+            </div>
+        </div>
+
     )
 }
 
