2cd35ab646a65dd6b2ef80e7165a79466a724700
Mihir Gupta <mihirgupta665@gmail.com>
2026-03-19 23:56:06 +0530

    Implemented toglling logic of dark and light mode

--- 13_Theme_Switcher_Context_API/src/App.jsx
+++ 13_Theme_Switcher_Context_API/src/App.jsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
+    
+    const [themeMode, setThemeMode] = useState("light")
+    const darkTheme = () => { setThemeMode("dark") }
+    const lightTheme = () => { setThemeMode("light") }
+
+    useEffect(() => {
+        document.querySelector("html").classList.remove("light", "dark")
+        document.querySelector("html").classList.add(themeMode)
+    }, [themeMode])
 
     return (
 
-        <div className="flex flex-wrap min-h-screen items-center">
-            <div className="w-full">
-                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
 
-                </div>
+        <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+            <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
 
-                <div className="w-full max-w-sm mx-auto">
+                    </div>
 
+                    <div className="w-full max-w-sm mx-auto">
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </ThemeProvider>
 
     )
 }
